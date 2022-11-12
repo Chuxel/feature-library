d5f1be4d95bc37cc6b4a00b440b25d456376f60e
Chuck Lantz <chuck_lantz@hotmail.com>
2022-11-12 02:02:33 +0000

    Add zsh

--- src/test-private-feature/install.sh
+++ src/test-private-feature/install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo 'echo "Yep, I executed!"' > /etc/bash.bashrc
+echo 'echo "Yep, I executed!"' >> /etc/bash.bashrc
+if [ -e /etc/zsh ]; then
+    echo 'echo "Yep, I executed!"' >> /etc/zsh/zshenv
+fi
